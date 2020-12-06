5b1e3dbbfa1fc338286aaa487a3d442a3df1b9fe
f0cus3d <stephen.d.ellis@gmail.com>
2020-12-05 16:55:59 -0800

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # AoC-2020-Rust
+
+My attempts at solving https://adventofcode.com/ using Rust.
+
+Feel free to comment or contrib.
---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() {
+    let mut expense_report: Vec<i64> = Vec::new();
+
+    if let Ok(lines) = read_lines("./input") {
+        for line in lines {
+            if let Ok(expense_item) = line {
+               let value  = expense_item.parse::<i64>().unwrap();
+               expense_report.push(value);
+            }           
+        }
+    }
+
+    for n in 0..expense_report.len(){
+        let mut x = 0;
+        while x < expense_report.len() {
+            let sum = expense_report[n] + expense_report[x];
+
+            if sum == 2020 {
+                println!("{:?} + {:?} = {:?}", &expense_report[n], &expense_report[x], &sum);
+                println!("ANSWER!: {:?}", expense_report[n] * expense_report[x]);
+            }
+            x += 1
+        }
+    }
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
