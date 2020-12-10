03084fc821318b4414ab7ebd3664644c77c0cb65
f0cus3d <stephen.d.ellis@gmail.com>
2020-12-10 13:37:48 -0800

    Comments

--- Day2/src/main.rs
+++ Day2/src/main.rs
@@ -3,6 +3,7 @@ use std::io::{self, BufRead};
 use std::path::Path;
 use std::collections::HashMap;
 
+// Debug for reasons....
 #[derive(Debug)]
 struct PassData{
     complex_max: u8,
@@ -13,20 +14,23 @@ struct PassData{
 
 fn main() {
 
+    // Count all the things and make that collection
     let mut compliance_count = 0;
     let mut pass_map = HashMap::new();
     let mut uid: u64 = 0;
 
+    // Read and process all the things
     if let Ok(lines) = read_lines("./input") {
         for line in lines {
             if let Ok(line_item) = line {
             let data = line_item.split_whitespace();
-
+            
+            // This is probably not needed but hey it works.
             let mut complexity_min_max: Vec<u8> = Vec::new();
             let mut complexity_char: Vec<String> = Vec::new();
             let mut password: Vec<String> = Vec::new();
 
-
+            // Another unintuative way at parsing text....
             for data_item in data {
                 if data_item.find("-") != None {
                     let complex_num_data = data_item.split("-");
@@ -40,7 +44,7 @@ fn main() {
                     password.push(data_item.to_string());
                 }
             }
-
+            // Way more crafty than it needs to be but again it works.
             let password_data = PassData{
                 complex_max: complexity_min_max.pop().unwrap(),
                 complex_min: complexity_min_max.pop().unwrap(),
@@ -49,6 +53,7 @@ fn main() {
 
             };
 
+            // Store based on UID, which will not be used later.
             pass_map.insert(uid, password_data);
 
             uid += 1;
@@ -58,7 +63,7 @@ fn main() {
         }
     }
 
-
+    // Check data based on the problem of part 1.
     for (_, complex_data) in &pass_map {
         let pass_count = &complex_data.pass.matches(&complex_data.complex_char)
                         .count()
@@ -73,7 +78,7 @@ fn main() {
         }
 
     }
-
+    // Out something of use to the term.
     println!("Passowrds Compliant: {:?}", compliance_count);
 
 }
